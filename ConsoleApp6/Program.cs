ba33c5fde4e67e5801d1b07cd0376389847974cf
jan.ptacek <jan.ptacek@VT1-16>
2022-02-28 13:23:27 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Probíhá hackování Ruska");
+
+for (int b = 0; b <= 100;b += 20)
+    {
+    Thread.Sleep(1000);
+        Console.WriteLine("Progress: " + b + "%");
+    }
+Console.WriteLine("Rusko úspěšně hacknuté");
